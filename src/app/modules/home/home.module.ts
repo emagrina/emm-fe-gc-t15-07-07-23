ddfd0e4443aa16060f18468ccbfbcfc73b9e048a
Ernest Magriñá <ernest8081@gmail.com>
2023-07-11 09:24:23 +0200

    fix: :bug: Fix 404 image home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,16 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '@shared/shared.module';
+
 import { HomeLayoutComponent } from './pages/home-layout/home-layout.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SharedModule } from '@shared/shared.module';
 import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { DetallePageComponent } from './pages/detalle-page/detalle-page.component';
 
 @NgModule({
-  declarations: [HomeLayoutComponent, HomePageComponent, CharactersPageComponent, AboutPageComponent, DetallePageComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
-  exports: [HomeLayoutComponent, HomePageComponent, CharactersPageComponent, AboutPageComponent, DetallePageComponent],
+  declarations: [
+    HomeLayoutComponent,
+    HomePageComponent,
+    CharactersPageComponent,
+    AboutPageComponent,
+    DetallePageComponent,
+  ],
+  imports: [CommonModule, RouterModule, SharedModule],
+  exports: [
+    HomeLayoutComponent,
+    HomePageComponent,
+    CharactersPageComponent,
+    AboutPageComponent,
+    DetallePageComponent,
+  ],
 })
 export class HomeModule {}
